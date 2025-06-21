bd772d65109b23f0ea0b9971904ee9a1ff236df9
Banukajanith2 <banukajanith2@gmail.com>
2025-06-21 07:49:19 +0530

    `Update main.jsx to import App component instead of PageLoad`

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import PageLoad from './components/PageLoad.jsx'
+import App from './App.jsx'
 import MoviePage from './pages/MoviePage.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
     <Routes>
-      <Route path='/' element={<PageLoad/>}/>
+      <Route path='/' element={<App />}/>
       <Route path="/movie/:slug" element={<MoviePage />} />
       <Route path='/404-Error'/>
     </Routes>
