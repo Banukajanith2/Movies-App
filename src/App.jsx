bebde1c10f556b956d9b2228bbed037b267a6962
Banukajanith2 <banukajanith2@gmail.com>
2025-06-22 16:35:07 +0530

    `Refactored code: removed hardcoded social media links and icons, replaced with reusable Footer component; added scrollOnClick prop to TrendingMovies component; updated CSS classes and styles.`

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Search from "./components/Search.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import { updateSearchCount } from "./FirestoreService.js"; //using Firestore as Backend
 import TrendingMovies from "./components/TrendingMovies.jsx";
+import Footer from "./components/Footer.jsx";
 
 //get api key to make the request
 const API_BASE_URL = "https://api.themoviedb.org/3";
@@ -97,9 +98,9 @@ const App = () => {
         <div className="wrapper">
           <header>
             <img src="./popcorn.png" alt="Hero Banner" />
-            <h1 className="text-gradient">EZ Movies.</h1>
+            <h1 className="text-gradient">EZ Movies</h1>
             <p className="text-desciption">
-              Discover movies and shows you'll love with just a few clicks!
+              Discover Your Favourite Movies Online
             </p>
             <Search
               searchTerm={searchTerm}
@@ -108,7 +109,7 @@ const App = () => {
             />
           </header>
           {/* Trending Movies Section with scroll SwiperJs */}
-          <TrendingMovies />
+          <TrendingMovies scrollOnClick={false}/>
           {/* Here we call isLoading and error message from the state. if both are false we load the movies from movieList */}
           <section className="all-movies">
             <h2 className="mb-5">Popular Movies</h2>
@@ -144,79 +145,7 @@ const App = () => {
               </button>
             ))}
           </div>
-
-          <footer className="bg-tranparent p-5 mt-10 rounded-2xl">
-            <div className="flex flex-col items-center gap-4">
-              <div className="social-icons">
-                <a
-                  href="https://facebook.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12a10 10 0 10-11.5 9.9v-7H8v-2.9h2.5v-2.2c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.5h-1.2c-1.2 0-1.6.8-1.6 1.5v1.9h2.8l-.4 2.9h-2.4v7A10 10 0 0022 12z" />
-                  </svg>
-                </a>
-                <a
-                  href="https://twitter.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22.46 6c-.77.35-1.6.59-2.46.7a4.27 4.27 0 001.88-2.37 8.53 8.53 0 01-2.7 1.03 4.25 4.25 0 00-7.24 3.87 12.06 12.06 0 01-8.77-4.45 4.23 4.23 0 001.32 5.68 4.22 4.22 0 01-1.93-.54v.05a4.25 4.25 0 003.41 4.17 4.27 4.27 0 01-1.92.07 4.25 4.25 0 003.96 2.94 8.52 8.52 0 01-6.29 1.75 12.03 12.03 0 006.49 1.9c7.78 0 12.03-6.45 12.03-12.03 0-.18-.01-.35-.02-.53A8.55 8.55 0 0024 4.56a8.39 8.39 0 01-2.54.7z" />
-                  </svg>
-                </a>
-                <a
-                  href="https://instagram.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M7.75 2A5.75 5.75 0 002 7.75v8.5A5.75 5.75 0 007.75 22h8.5A5.75 5.75 0 0022 16.25v-8.5A5.75 5.75 0 0016.25 2h-8.5zm0 1.5h8.5c2.35 0 4.25 1.9 4.25 4.25v8.5c0 2.35-1.9 4.25-4.25 4.25h-8.5A4.25 4.25 0 013.5 16.25v-8.5C3.5 5.9 5.4 4 7.75 4zM12 7a5 5 0 100 10 5 5 0 000-10zm0 1.5a3.5 3.5 0 110 7 3.5 3.5 0 010-7zm5.5-.75a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z" />
-                  </svg>
-                </a>
-                <a
-                  href="https://linkedin.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8.5h5v15H0v-15zM7.5 8.5h4.7v2.1h.1c.66-1.2 2.3-2.4 4.7-2.4 5 0 5.9 3.3 5.9 7.6v8.2h-5v-7.3c0-1.8-.03-4.1-2.5-4.1-2.5 0-2.9 2-2.9 4v7.4h-5v-15z" />
-                  </svg>
-                </a>
-              </div>
-              <p className="text-gray-100 font-light text-xs">
-                © 2025 EZ Movies - By{" "}
-                <a
-                  href="https://github.com/Banukajanith2"
-                  target="_blank"
-                  className="underline hover:no-underline"
-                >
-                  Banuka Janith
-                </a>
-              </p>
-            </div>
-          </footer>
+          <Footer/>
         </div>
       </main>
     </>
--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -9,8 +9,15 @@ import "swiper/css/pagination";
 import MovieCard from "./MovieCard";
 import Spinner from "./Spinner";
 
-const TrendingMovies = () => {
+const TrendingMovies = ({scrollOnClick = false }) => {
   const [trendingMovies, setTrendingMovies] = useState([]);
+
+  //scroll
+  const handleClick = () => {
+    if (scrollOnClick) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
 
   // Fetch trending movies from Firestore
   const loadTrendingMovies = async () => {
@@ -29,18 +36,20 @@ const TrendingMovies = () => {
   }, []);
 
   return (
-    <section className="trending-movies mt-10 sm:p-0 pl-2 pr-2 ">
+    <section className="trending-movies mt-10 sm:p-0 pl-1 pr-1 ">
       <h2 className="mb-5 sm:text-3xl font-semibold">Trending Movies</h2>
 
       {trendingMovies.length > 0 ? (
         <div className="relative">
           <Swiper
+            onClick={handleClick}
             modules={[Navigation, Pagination, Autoplay]}
             spaceBetween={20}
             slidesPerView={2}
             loop={trendingMovies.length > 5}
+            speed={1000}
             autoplay={{
-              delay: 2000,
+              delay: 1000,
               disableOnInteraction: false,
               pauseOnMouseEnter: true,
             }}
@@ -56,7 +65,7 @@ const TrendingMovies = () => {
               768: { slidesPerView: 4 },
               1024: { slidesPerView: 5 },
             }}
-            className="w-full  h-[400px]"
+            className="w-full  h-[400px] animate-slide-up"
           >
             {trendingMovies.map((movie, index) => (
               <SwiperSlide key={movie.id}>
@@ -66,7 +75,7 @@ const TrendingMovies = () => {
           </Swiper>
 
           {/* Custom Left Arrow */}
-          <button className="custom-prev absolute left-1 sm:-left-5 top-40 -translate-y-1/2 z-10 bg-indigo-500 hover:bg-indigo-600 p-2 rounded-full text-white shadow-lg">
+          <button className="custom-prev absolute -left-1 sm:-left-5 top-40 -translate-y-1/2 z-10 bg-indigo-400 hover:bg-indigo-600 p-2 rounded-full text-white shadow-lg transition3s">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
             </svg>
@@ -74,7 +83,7 @@ const TrendingMovies = () => {
           </button>
 
           {/* Custom Right Arrow */}
-          <button className="custom-next absolute right-1 sm:-right-5 top-40 -translate-y-1/2 z-10 bg-indigo-500 hover:bg-indigo-600 p-2 rounded-full text-white shadow-lg">
+          <button className="custom-next absolute -right-1 sm:-right-5 top-40 -translate-y-1/2 z-10 bg-indigo-400 hover:bg-indigo-600 p-2 rounded-full text-white shadow-lg transition3s">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </svg>
