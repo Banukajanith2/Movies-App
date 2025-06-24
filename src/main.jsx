daaaad09dca11808e8726729aa795313bfe0915b
Banukajanith2 <banukajanith2@gmail.com>
2025-06-24 20:20:04 +0530

    `feat: updated main.jsx and vite.config.js with lazy loading and suspense`

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import MoviePage from './pages/MoviePage.jsx'
-import './index.css'
-import ErrorPage from './pages/404-ErrorPage.jsx'
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import App from './App.jsx';
+import './index.css';
+
+// Lazy-loaded pages
+const MoviePage = lazy(() => import('./pages/MoviePage.jsx'));
+const ErrorPage = lazy(() => import('./pages/404-ErrorPage.jsx'));
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
-    <Routes>
-      <Route path='/' element={<App />}/>
-      <Route path="/movie/:slug" element={<MoviePage />} />
-      <Route path="*" element={<ErrorPage/>}/>
-      <Route path="/404-Error" element={<ErrorPage/>}/>
-
-    </Routes>
+    <Suspense >
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/movie/:slug' element={<MoviePage />} />
+        <Route path='/404-Error' element={<ErrorPage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </Suspense>
   </HashRouter>
-)
-
+);
