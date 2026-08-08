4474624efde58c6f0dc0110cf76311bb30f82770
Banukajanith2 <banukajanith2@gmail.com>
2026-08-08 21:05:59 +0530

    Updates

---
+++ assets/useDocumentTitle-Bb3LkE3s.js
@@ -0,0 +1,1 @@
+import{x as t}from"./index-CuS_FoQP.js";const o="EZ Movies - Watch HD Movies & TV Shows Online",c=e=>{t.useEffect(()=>(document.title=e?`${e} • EZ Movies`:o,()=>{document.title=o}),[e])};export{c as u};
